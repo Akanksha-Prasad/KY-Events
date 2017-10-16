c2ad3915894dc2298d5116e7db1a2e828f5cc27e
Akanksha-Prasad <akanksha.prasad.met16@itbhu.ac.in>
2017-10-16 18:26:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,150 @@
+function myFunction1() 
+{
+
+	document.getElementById("first").className="fadeIn";
+        document.getElementById("second").className="fadeOut";
+	document.getElementById("third").className="fadeOut";
+	document.getElementById("fourth").className="fadeOut";
+
+}
+
+function myFunction2() 
+{
+	document.getElementById("first").className="fadeOut";
+        document.getElementById("second").className="fadeIn";
+	document.getElementById("third").className="fadeOut";
+	document.getElementById("fourth").className="fadeOut";
+}
+
+function myFunction3() 
+{
+	document.getElementById("first").className="fadeOut";
+        document.getElementById("second").className="fadeOut";
+	document.getElementById("third").className="fadeIn";
+	document.getElementById("fourth").className="fadeOut";
+
+}
+
+function myFunction4() 
+{
+	document.getElementById("first").className="fadeOut";
+        document.getElementById("second").className="fadeOut";
+	document.getElementById("third").className="fadeOut";
+	document.getElementById("fourth").className="fadeIn";
+
+}
+
+
+/* function myFunction1() 
+{
+	if(document.getElementById("first").style.display==="block") {
+
+	document.getElementById("first").className="fadeIn";
+	document.getElementById("second").style.display="none";
+	document.getElementById("third").style.display="none";
+	document.getElementById("fourth").style.display="none";
+	}
+
+	else if(document.getElementById("second").style.display==="block") {
+	document.getElementById("first").style.display="block";
+	document.getElementById("first").className="fadeIn";
+	document.getElementById("second").className="fadeOut";
+	document.getElementById("second").style.display="none";
+	document.getElementById("third").style.display="none";
+	document.getElementById("fourth").style.display="none";
+	}
+	else if(document.getElementById("third").style.display==="block") {
+	document.getElementById("first").style.display="block";
+	document.getElementById("first").className="fadeIn";
+	document.getElementById("second").style.display="none";
+	document.getElementById("third").className="fadeOut";
+	document.getElementById("fourth").style.display="none";
+	}
+	else if(document.getElementById("fourth").style.display==="block") {
+	document.getElementById("first").style.display="block";
+	document.getElementById("first").className="fadeIn";
+	document.getElementById("second").style.display="none";
+	document.getElementById("third").style.display="none";
+	document.getElementById("fourth").className="fadeOut";
+	}
+}
+
+function myFunction2() 
+{
+	if(document.getElementById("first").style.display==="block") {
+	document.getElementById("first").className="fadeOut";
+	document.getElementById("second").style.display="block";
+	document.getElementById("second").className="fadeIn";
+	document.getElementById("third").style.display="none";
+	document.getElementById("fourth").style.display="none";
+	}
+	else if(document.getElementById("third").style.display==="block") {
+	document.getElementById("first").style.display="none";
+	document.getElementById("second").style.display="block";
+	document.getElementById("second").className="fadeIn";
+	document.getElementById("third").className="fadeOut";
+	document.getElementById("fourth").style.display="none";
+	}
+	else if(document.getElementById("fourth").style.display==="block") {
+	document.getElementById("first").style.display="none";
+	document.getElementById("second").style.display="block";
+	document.getElementById("second").className="fadeIn";
+	document.getElementById("third").style.display="none";
+	document.getElementById("fourth").className="fadeOut";
+	}
+}
+
+
+function myFunction3() 
+{
+	if(document.getElementById("first").style.display==="block") {
+	document.getElementById("first").className="fadeOut";
+	document.getElementById("second").style.display="none";
+	document.getElementById("third").style.display="block";
+	document.getElementById("third").className="fadeIn";
+	document.getElementById("fourth").style.display="none";
+	}
+	else if(document.getElementById("second").style.display==="block") {
+	document.getElementById("first").style.display="none";
+	document.getElementById("second").className="fadeOut";
+	document.getElementById("second").style.display="none";
+	document.getElementById("third").style.display="block";
+	document.getElementById("third").className="fadeIn";
+	document.getElementById("fourth").style.display="none";
+	}
+	else if(document.getElementById("fourth").style.display==="block") {
+	document.getElementById("first").style.display="none";
+	document.getElementById("second").style.display="none";
+	document.getElementById("third").style.display="block";
+	document.getElementById("third").className="fadeIn";
+	document.getElementById("fourth").className="fadeOut";
+	}
+}
+
+function myFunction4() 
+{
+	if(document.getElementById("first").style.display==="block") {
+	document.getElementById("first").className="fadeOut";
+	document.getElementById("second").style.display="none";
+	document.getElementById("third").style.display="none";
+	document.getElementById("fourth").style.display="block";
+	document.getElementById("fourth").className="fadeIn";
+	}
+	else if(document.getElementById("second").style.display==="block") {
+	document.getElementById("first").style.display="none";
+	document.getElementById("second").className="fadeOut";
+	document.getElementById("second").style.display="none";
+	document.getElementById("third").style.display="none";
+	document.getElementById("fourth").style.display="block";
+	document.getElementById("fourth").className="fadeIn";
+	}
+	else if(document.getElementById("third").style.display==="block") {
+	document.getElementById("first").style.display="none";
+	document.getElementById("second").style.display="none";
+	document.getElementById("third").className="fadeOut";
+	document.getElementById("fourth").style.display="block";
+	document.getElementById("fourth").className="fadeIn";
+	}
+	
+}
+*/
